e17116325c9093f3c9cc7b86b024598d9ed75fb1
Gabriel D. M <gmanoel057@gmail.com>
2021-03-19 08:25:21 -0300

    Init app

---
+++ dist.js
@@ -0,0 +1,9 @@
+function Menu() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "bg-dark"
+  }, /*#__PURE__*/React.createElement("p", {
+    className: "text-light"
+  }, "Hello World"));
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(Menu, null), document.getElementById('root'));
---
+++ main.js
@@ -0,0 +1,12 @@
+function Menu() {
+    return(
+        <div className="bg-dark">
+            <p className="text-light">Hello World</p>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <Menu />,
+    document.getElementById('root')
+)
