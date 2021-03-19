4daad45c1226116201857ef92c18f2d3facaeace
Gabriel D. M <gmanoel057@gmail.com>
2021-03-19 11:57:08 -0300

    Apresentation finished

--- dist.js
+++ dist.js
@@ -1,9 +1,57 @@
+// function MyImg() {
+//     return(
+//         <div className="bg-myphoto">
+//         </div>
+//     )
+// }
+function MyHead() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "card container text-white bg-dark mb-3"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "row g-0"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "image col-md-4"
+  }, /*#__PURE__*/React.createElement("img", {
+    className: "myphoto",
+    src: "./src/img/myphoto.jpg"
+  })), /*#__PURE__*/React.createElement("div", {
+    className: "text-on-card col-md-8"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("h5", {
+    className: "card-title"
+  }, /*#__PURE__*/React.createElement("p", {
+    className: "display-5"
+  }, "LordMan0"), /*#__PURE__*/React.createElement("p", {
+    className: "display-6"
+  }, "Gabriel Domingues Manoel")), /*#__PURE__*/React.createElement("p", {
+    className: "card-text"
+  }, /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "E-mail: gabrieldmanoel20@gmail.com"), /*#__PURE__*/React.createElement("li", null, "Telefone: (19) 99459-9748"))), /*#__PURE__*/React.createElement("p", {
+    className: "card-text"
+  }, /*#__PURE__*/React.createElement("small", {
+    className: "text-muted"
+  }, "Last updated 19/03/2021")), /*#__PURE__*/React.createElement("div", {
+    className: "row g-0"
+  }, /*#__PURE__*/React.createElement("a", {
+    className: "col-1",
+    href: "https://github.com/LordMan0"
+  }, /*#__PURE__*/React.createElement("img", {
+    className: "github",
+    src: "./src/icons/github-icon.png"
+  })), /*#__PURE__*/React.createElement("a", {
+    className: "col-1",
+    href: "https://www.linkedin.com/in/gabrieldmanoel/"
+  }, /*#__PURE__*/React.createElement("img", {
+    className: "linkedin",
+    src: "./src/icons/linkedin-icon.png",
+    href: ""
+  })))))));
+}
+
 function Menu() {
   return /*#__PURE__*/React.createElement("div", {
-    className: "bg-dark"
-  }, /*#__PURE__*/React.createElement("p", {
-    className: "text-light"
-  }, "Hello World"));
+    className: "main container"
+  }, /*#__PURE__*/React.createElement(MyHead, null));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(Menu, null), document.getElementById('root'));
--- main.js
+++ main.js
@@ -1,7 +1,50 @@
+// function MyImg() {
+//     return(
+//         <div className="bg-myphoto">
+            
+//         </div>
+//     )
+// }
+
+function MyHead() {
+    return(
+        <div className="card container text-white bg-dark mb-3">
+            <div className="row g-0">
+                <div className="image col-md-4">
+                    <img className="myphoto" src="./src/img/myphoto.jpg" />
+                </div>
+                <div className="text-on-card col-md-8">
+                <div className="card-body">
+                    <h5 className="card-title">
+                        <p className="display-5">LordMan0</p>
+                        <p className="display-6">Gabriel Domingues Manoel</p>
+                    </h5>
+                    <p className="card-text">
+                        <ul>
+                            <li>E-mail: gabrieldmanoel20@gmail.com</li>
+                            <li>Telefone: (19) 99459-9748</li>
+                        </ul>   
+                    </p>
+                    <p className="card-text"><small className="text-muted">Last updated 19/03/2021</small></p>
+                    <div className="row g-0">
+                        <a className="col-1" href="https://github.com/LordMan0">
+                        <img className="github" src="./src/icons/github-icon.png"/>
+                        </a>
+                        <a className="col-1" href="https://www.linkedin.com/in/gabrieldmanoel/">
+                        <img className="linkedin" src="./src/icons/linkedin-icon.png" href=""/>
+                        </a>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 function Menu() {
     return(
-        <div className="bg-dark">
-            <p className="text-light">Hello World</p>
+        <div className="main container">
+            <MyHead />
         </div>
     )
 }
