4547a4ccc841dc5c4d8d8c3f010047cae3086cd2
Gabriel D. M <gmanoel057@gmail.com>
2021-03-19 17:01:47 -0300

    Web version 1.0

--- dist.js
+++ dist.js
@@ -1,9 +1,3 @@
-// function MyImg() {
-//     return(
-//         <div className="bg-myphoto">
-//         </div>
-//     )
-// }
 function MyHead() {
   return /*#__PURE__*/React.createElement("div", {
     className: "card container text-white bg-dark mb-3"
@@ -13,7 +7,7 @@ function MyHead() {
     className: "image col-md-4"
   }, /*#__PURE__*/React.createElement("img", {
     className: "myphoto",
-    src: "./src/img/myphoto.jpg"
+    src: "./src/wallpaper/devwallpaper.png"
   })), /*#__PURE__*/React.createElement("div", {
     className: "text-on-card col-md-8"
   }, /*#__PURE__*/React.createElement("div", {
@@ -26,12 +20,12 @@ function MyHead() {
     className: "display-6"
   }, "Gabriel Domingues Manoel")), /*#__PURE__*/React.createElement("p", {
     className: "card-text"
-  }, /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "E-mail: gabrieldmanoel20@gmail.com"), /*#__PURE__*/React.createElement("li", null, "Telefone: (19) 99459-9748"))), /*#__PURE__*/React.createElement("p", {
+  }, /*#__PURE__*/React.createElement("li", null, "E-mail: gabrieldmanoel20@gmail.com"), /*#__PURE__*/React.createElement("li", null, "Telefone: (19) 99459-9748")), /*#__PURE__*/React.createElement("p", {
     className: "card-text"
   }, /*#__PURE__*/React.createElement("small", {
     className: "text-muted"
-  }, "Last updated 19/03/2021")), /*#__PURE__*/React.createElement("div", {
-    className: "row g-0"
+  }, "\xDAltima atualiza\xE7\xE3o 19/03/2021")), /*#__PURE__*/React.createElement("div", {
+    className: "row g-5"
   }, /*#__PURE__*/React.createElement("a", {
     className: "col-1",
     href: "https://github.com/LordMan0"
@@ -43,15 +37,105 @@ function MyHead() {
     href: "https://www.linkedin.com/in/gabrieldmanoel/"
   }, /*#__PURE__*/React.createElement("img", {
     className: "linkedin",
-    src: "./src/icons/linkedin-icon.png",
-    href: ""
+    src: "./src/icons/linkedin-icon.png"
   })))))));
+}
+
+function ButtonForApps() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "scrollBtn container text-center"
+  }, /*#__PURE__*/React.createElement("button", {
+    type: "button",
+    className: "btn btn-outline-light btn-lg",
+    onClick: () => {
+      scrollTo({
+        top: 935,
+        behavior: 'smooth'
+      });
+    }
+  }, "Ver aplica\xE7\xF5es"));
 }
 
 function Menu() {
   return /*#__PURE__*/React.createElement("div", {
-    className: "main container"
-  }, /*#__PURE__*/React.createElement(MyHead, null));
+    className: "main"
+  }, /*#__PURE__*/React.createElement(MyHead, null), /*#__PURE__*/React.createElement(ButtonForApps, null));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(Menu, null), document.getElementById('root'));
+
+function ListPanel() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "container row row-cols-1 row-cols-md-2 g-4"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "col text-center"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "app-card card mx-auto text-white bg-dark"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "./src/icons/MusicQuizIcon.jpg",
+    className: "card-img-top mx-auto"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("h5", {
+    className: "card-title"
+  }, "Music Quiz"), /*#__PURE__*/React.createElement("p", {
+    className: "card-text"
+  }, "Aplica\xE7\xE3o de quiz sobre conhecimento musicais (em especial m\xFAsica internacional)."), /*#__PURE__*/React.createElement("a", {
+    href: "https://lordman0-music-quiz.netlify.app/",
+    className: "btn btn-outline-secondary"
+  }, "Ver aplica\xE7\xE3o")))), /*#__PURE__*/React.createElement("div", {
+    className: "col text-center"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "app-card card mx-auto text-white bg-dark"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "./src/icons/TodoIcon.jpg",
+    className: "card-img-top mx-auto"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("h5", {
+    className: "card-title"
+  }, "To-do List"), /*#__PURE__*/React.createElement("p", {
+    className: "card-text"
+  }, "Aplica\xE7\xE3o de lista de afazeres para anotar suas tarefas do dia a dia."), /*#__PURE__*/React.createElement("a", {
+    href: "https://lordman0-todo-list.netlify.app/",
+    className: "btn btn-outline-secondary"
+  }, "Ver aplica\xE7\xE3o")))), /*#__PURE__*/React.createElement("div", {
+    className: "col text-center"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "app-card card mx-auto text-white bg-dark"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "./src/icons/WAppIcon.jpg",
+    className: "card-img-top mx-auto"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("h5", {
+    className: "card-title"
+  }, "Weather App"), /*#__PURE__*/React.createElement("p", {
+    className: "card-text"
+  }, "Aplica\xE7\xE3o de informar o clima de qualquer cidade do mundo (al\xE9m de tamb\xE9m mostrar se est\xE1 de dia ou de noite)."), /*#__PURE__*/React.createElement("a", {
+    href: "https://lordman0-weather-app.netlify.app/",
+    className: "btn btn-outline-secondary"
+  }, "Ver aplica\xE7\xE3o")))), /*#__PURE__*/React.createElement("div", {
+    className: "col text-center"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "app-card card mx-auto text-white bg-dark"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "./src/icons/CurrencyConverterIcon.jpg",
+    className: "card-img-top mx-auto"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("h5", {
+    className: "card-title"
+  }, "Currency Converter"), /*#__PURE__*/React.createElement("p", {
+    className: "card-text"
+  }, "Aplica\xE7\xE3o para obter as taxas de c\xE2mbio de acordo com o moeda desejada."), /*#__PURE__*/React.createElement("a", {
+    href: "https://lordman0-currency-converter.netlify.app/",
+    className: "btn btn-outline-secondary"
+  }, "Ver aplica\xE7\xE3o")))));
+}
+
+function Panel() {
+  return /*#__PURE__*/React.createElement(ListPanel, null);
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(Panel, null), document.getElementById('panel'));
--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-// function MyImg() {
-//     return(
-//         <div className="bg-myphoto">
-            
-//         </div>
-//     )
-// }
-
 function MyHead() {
     return(
         <div className="card container text-white bg-dark mb-3">
             <div className="row g-0">
                 <div className="image col-md-4">
-                    <img className="myphoto" src="./src/img/myphoto.jpg" />
+                    <img className="myphoto" src="./src/wallpaper/devwallpaper.png" />
                 </div>
                 <div className="text-on-card col-md-8">
                 <div className="card-body">
@@ -20,18 +12,16 @@ function MyHead() {
                         <p className="display-6">Gabriel Domingues Manoel</p>
                     </h5>
                     <p className="card-text">
-                        <ul>
                             <li>E-mail: gabrieldmanoel20@gmail.com</li>
-                            <li>Telefone: (19) 99459-9748</li>
-                        </ul>   
+                            <li>Telefone: (19) 99459-9748</li>  
                     </p>
-                    <p className="card-text"><small className="text-muted">Last updated 19/03/2021</small></p>
-                    <div className="row g-0">
+                    <p className="card-text"><small className="text-muted">Última atualização 19/03/2021</small></p>
+                    <div className="row g-5">
                         <a className="col-1" href="https://github.com/LordMan0">
                         <img className="github" src="./src/icons/github-icon.png"/>
                         </a>
                         <a className="col-1" href="https://www.linkedin.com/in/gabrieldmanoel/">
-                        <img className="linkedin" src="./src/icons/linkedin-icon.png" href=""/>
+                        <img className="linkedin" src="./src/icons/linkedin-icon.png" />
                         </a>
                     </div>
                 </div>
@@ -41,10 +31,23 @@ function MyHead() {
     )
 }
 
+function ButtonForApps() {
+    return(
+        <div className="scrollBtn container text-center">
+            <button type="button" className="btn btn-outline-light btn-lg"
+                onClick={ () => { scrollTo({ top: 935, behavior: 'smooth' }) } }
+            >
+                Ver aplicações
+            </button>
+        </div>
+    )
+}
+
 function Menu() {
     return(
-        <div className="main container">
+        <div className="main">
             <MyHead />
+            <ButtonForApps />
         </div>
     )
 }
@@ -52,4 +55,62 @@ function Menu() {
 ReactDOM.render(
     <Menu />,
     document.getElementById('root')
+)
+
+function ListPanel() {
+    return(
+        <div className="container row row-cols-1 row-cols-md-2 g-4">
+            <div className="col text-center">
+                <div className="app-card card mx-auto text-white bg-dark">
+                    <img src="./src/icons/MusicQuizIcon.jpg" className="card-img-top mx-auto" />
+                    <div className="card-body">
+                        <h5 className="card-title">Music Quiz</h5>
+                        <p className="card-text">Aplicação de quiz sobre conhecimento musicais (em especial música internacional).</p>
+                        <a href="https://lordman0-music-quiz.netlify.app/" className="btn btn-outline-secondary">Ver aplicação</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col text-center">
+                <div className="app-card card mx-auto text-white bg-dark">
+                    <img src="./src/icons/TodoIcon.jpg" className="card-img-top mx-auto" />
+                    <div className="card-body">
+                        <h5 className="card-title">To-do List</h5>
+                        <p className="card-text">Aplicação de lista de afazeres para anotar suas tarefas do dia a dia.</p>
+                        <a href="https://lordman0-todo-list.netlify.app/" className="btn btn-outline-secondary">Ver aplicação</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col text-center">
+                <div className="app-card card mx-auto text-white bg-dark">
+                    <img src="./src/icons/WAppIcon.jpg" className="card-img-top mx-auto" />
+                    <div className="card-body">
+                        <h5 className="card-title">Weather App</h5>
+                        <p className="card-text">Aplicação de informar o clima de qualquer cidade do mundo (além de também mostrar se está de dia ou de noite).</p>
+                        <a href="https://lordman0-weather-app.netlify.app/" className="btn btn-outline-secondary">Ver aplicação</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col text-center">
+                <div className="app-card card mx-auto text-white bg-dark">
+                    <img src="./src/icons/CurrencyConverterIcon.jpg" className="card-img-top mx-auto" />
+                    <div className="card-body">
+                        <h5 className="card-title">Currency Converter</h5>
+                        <p className="card-text">Aplicação para obter as taxas de câmbio de acordo com o moeda desejada.</p>
+                        <a href="https://lordman0-currency-converter.netlify.app/" className="btn btn-outline-secondary">Ver aplicação</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Panel() {
+    return(
+        <ListPanel />
+    )
+}
+
+ReactDOM.render(
+    <Panel />,
+    document.getElementById('panel')
 )
